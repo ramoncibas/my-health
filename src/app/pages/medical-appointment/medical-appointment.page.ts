63a2adf966b2161328fceed56613f4cc91c1af53
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-02 20:57:14 -0300

    rendering data from the database

--- src/app/pages/medical-appointment/medical-appointment.page.ts
+++ src/app/pages/medical-appointment/medical-appointment.page.ts
@@ -5,9 +5,9 @@ import { FormsModule } from '@angular/forms';
 
 import { PopoverController } from '@ionic/angular';
 
-// import { Subscription } from 'rxjs';
-// import { Appointment } from 'src/app/interfaces/appointment';
-// import { AppointmentService } from 'src/app/services/auth/appointment.service';
+import { Subscription } from 'rxjs';
+import { Doctor } from 'src/app/interfaces/doctor';
+import { DoctorService } from 'src/app/services/doctor.service';
 
 import { ListItemsComponent } from '../../components/list-items/list-items.component';
 import { FilterDoctorsComponent } from 'src/app/components/Popovers/filter-doctors/filter-doctors.component';
@@ -26,68 +26,25 @@ import { FilterDoctorsComponent } from 'src/app/components/Popovers/filter-docto
   declarations: [ListItemsComponent, FilterDoctorsComponent],
 })
 export class MedicalAppointmentPage implements OnInit {
+  private doctors = new Array<Doctor>();
+  private doctorsSubscription: Subscription;
 
-  constructor(
-    public popoverController: PopoverController
-  ) {}
-
-  // Fake data
-  private appointments = [
-    {
-      name: 'Dr. Marcio',
-      specialty: ['Clinico Geral', 'Cirurgião', 'Pediatra'],
-      picture:
-        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-      description: 'Consulta de rotina.',
-      price: 150,
-      timeAvaliable: '2021-05-01T15:43:40.394Z',
-    },
-    {
-      name: 'Dr. Tanga',
-      specialty: ['Clinico Geral'],
-      picture:
-        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-      description: 'Consulta de rotina.',
-      price: null,
-      timeAvaliable: '2021-05-01T15:43:40.394Z',
-    },
-    {
-      name: 'Dr. Silva',
-      specialty: ['Pediatra'],
-      picture:
-        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-      description: 'Consulta de rotina.',
-      price: null,
-      timeAvaliable: '2021-05-01T15:43:40.394Z',
-    },
-    {
-      name: 'Dr. Mauricio',
-      specialty: ['Cirurgião'],
-      picture:
-        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-      description: 'Consulta de rotina.',
-      price: null,
-      timeAvaliable: '2021-05-01T15:43:40.394Z',
-    },
-  ];
-
-  // private appointments =  new Array<Appointment>();
-  // private appointmentsSubscription: Subscription;
-
-  // constructor( private appointmentsService: AppointmentService ) {
-  //   this.appointmentsSubscription = this.appointmentsService.getAppointments().subscribe(data => {
-  //     this.appointments = data;
-  //   });
-  // }
+  constructor( 
+    public popoverController: PopoverController,  
+    private doctorsService: DoctorService,
+  ) {
+    this.doctorsSubscription = this.doctorsService.getDoctors().subscribe(data => {
+      this.doctors = data;
+    });
+  }
 
   ngOnInit() {}
 
-  // // Destroy listen
-  // ngOnDestroy() {
-  //   this.appointmentsSubscription.unsubscribe();
-  // }
-
-  //appointments:Array<Appointment>  
+  // Destroy listen
+  ngOnDestroy() {
+    this.doctorsSubscription.unsubscribe();
+  }
+  
   // Filter by name
   searchByName(event:any) {
     const query = event.target.value.toLowerCase();
