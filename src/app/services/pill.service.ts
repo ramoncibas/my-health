f04bd2cda5f709f6208a86d5de2247736bbd301a
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-06 22:11:36 -0300

    Update pill.service.ts

--- src/app/services/pill.service.ts
+++ src/app/services/pill.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
 import * as firebase from 'firebase/app';
-import { map } from "rxjs/operators";
+import { map, switchMap } from "rxjs/operators";
 
-import { Pills } from '../interfaces/pills';
+import { AuthService } from './auth.service';
+import { Pill } from '../interfaces/pills';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PillService {
-  private pillCollection: AngularFirestoreCollection<Pills>;
+  private pillCollection: AngularFirestoreCollection<Pill>;
 
   constructor(
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private authService: AuthService
   ) { 
-    this.pillCollection = this.afs.collection<Pills>('pills');
+    this.pillCollection = this.afs.collection<Pill>('pills');
   }
 
-  getPills() {
+  /**   
+   * @return all pills from the database
+   */   
+  getAllPills() {
     return this.pillCollection.snapshotChanges().pipe(
       map(actions => {
         return actions.map(item => {
@@ -30,18 +36,49 @@ export class PillService {
     )
   }
 
-  pillsInsert(data: Pills) {
-    return this.afs.collection('pills').add({
-      uid: data.uid,
-      name: data.name,
-      description: data.description,
-      price: data.price,
-      picture: data.picture,
-      brande: data.brand,
-      promotion: data.promotion,
+  /**
+   * @return return my pills 
+   */
+  getMyPills() {
+    return this.afs.collection<any>('user_pills').snapshotChanges().pipe(
+      map(action => {
+        return action.map(item => {
+          const data = item.payload.doc.data();
+          const id = item.payload.doc.id;
+          
+          return { id, ...data };
+        })
+      })
+    )
+  }
+
+  /**
+   * Function that stores user purchase data
+   * @param {Pill} pill pills data
+   *
+   */ 
+  buyPills(pill: Pill) {
+    const userUid = this.authService.currentUser.uid;
+
+    return this.afs.collection('user_pills').add({
+      userUid: userUid,
+      pill: pill,
+    })
+  }
+  
+  /**
+   * Insert a pill in the database
+   * @param {Pill} pill drug information
+   */
+  pillsInsert(pill: Pill) {
+    return this.pillCollection.add({      
+      name: pill.name,
+      description: pill.description,
+      price: pill.price,
+      picture: pill.picture,
+      brand: pill.brand,
+      promotion: pill.promotion,
       createdAt: firebase.default.firestore.FieldValue.serverTimestamp(),
     })
   }
-
-
 }
