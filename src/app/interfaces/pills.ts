fb2424aca58561e3b85a2e794458d5a29168039f
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-12 21:00:37 -0300

    Update pills.ts

--- src/app/interfaces/pills.ts
+++ src/app/interfaces/pills.ts
@@ -1,9 +1,24 @@
-export interface Pill {    
-    name?: string;
-    description?: string;
-    price?: string;
-    picture?: string;
-    brand?: string;
-    promotion?: number;
-    createdAt: any; 
+export interface Pill {
+  name?: string;
+  description?: string;
+  price?: string;
+  picture?: string;
+  brand?: string;
+  promotion?: number;
+  createdAt: any;
+}
+
+export interface UserPills {
+  pill?: {
+    amount: number,
+    brand: string,
+    createdAt: any,
+    description: string,
+    id: string,
+    name: string,
+    picture: string,
+    price: string,
+    promotion?: any,
+  };
+  boughtBy?: string;
 }
