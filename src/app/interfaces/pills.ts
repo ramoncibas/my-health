1db015b6f6cd231820aac481967c0cd0d3e57bae
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-17 21:42:51 -0300

    Update pills.ts

--- src/app/interfaces/pills.ts
+++ src/app/interfaces/pills.ts
@@ -1,24 +1,49 @@
 export interface Pill {
+  id?: string;
   name?: string;
   description?: string;
   price?: string;
   picture?: string;
   brand?: string;
   promotion?: number;
-  createdAt: any;
+  createdAt: firebase.default.firestore.Timestamp;
 }
 
 export interface UserPills {
-  pill?: {
-    amount: number,
-    brand: string,
-    createdAt: any,
-    description: string,
-    id: string,
-    name: string,
-    picture: string,
-    price: string,
-    promotion?: any,
-  };
-  boughtBy?: string;
+  pill?: [
+    {
+      id?: string;
+      name?: string;
+      brand?: string;
+      description?: string;
+      picture?: string;
+      amount?: any;
+      price: string;
+      promotion?: any;
+      boughtBy?: string;
+      createdAt?: firebase.default.firestore.Timestamp;
+    }
+  ];
+  vaccine?: [
+    {
+      id: string;
+      name: string;
+      brand: string;
+      createdAt: firebase.default.firestore.Timestamp;
+    }
+  ];
+  check_up?: [
+    {
+      id: string;
+      name: string;
+      doctor: { doctorId?: string; doctorSpecialty: any };
+      createdAt: firebase.default.firestore.Timestamp;
+    }
+  ];
+  professionals?: [
+    {
+      id: string;
+      doctor: { doctor: any };
+    }
+  ];
 }
