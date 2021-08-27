0383bc34ffb339c90f3825f4ef920c720d38ab7f
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-26 22:20:26 -0300

    some changes

--- src/app/components/Modals/modal-my-health/modal-my-health.page.ts
+++ src/app/components/Modals/modal-my-health/modal-my-health.page.ts
@@ -1,20 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
+import { Subscription } from 'rxjs';
+import { UserHealthService } from 'src/app/services/user-health.service';
 @Component({
   selector: 'app-modal-my-health',
   templateUrl: './modal-my-health.page.html',
   styleUrls: ['./modal-my-health.page.scss'],
 })
-export class ModalMyHealthPage implements OnInit {
-  // Fake data  
+export class ModalMyHealthPage implements OnInit {  
   @Input() data: any;
   @Input() type: any;
 
-  constructor(private modalControll: ModalController) { }
+  constructor(
+    private modalControll: ModalController,
+  ) {}
 
   ngOnInit() {
-    console.log(this.data)
+    console.table(this.data)
   }
   
   async closeModal() {
@@ -22,7 +24,7 @@ export class ModalMyHealthPage implements OnInit {
   }
  
   async shareItem(data) {
-    console.log(data);
+    console.log(data);    
   }
 
   async deleteItem(data) {
