c43eb00ffd444482fb8974c5cfabac9893bc33cf
Ramon Cibas <ramon.cibas@hotmail.com>
2021-08-30 22:27:53 -0300

    passing data

--- src/app/pages/my-appointment/my-appointment.page.ts
+++ src/app/pages/my-appointment/my-appointment.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { ModalController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { ModalMyHealthPage } from 'src/app/components/Modals/modal-my-health/modal-my-health.page';
-import { UserPills } from 'src/app/interfaces/pills';
-import { PillService } from 'src/app/services/pill.service';
 import { UserHealthService } from 'src/app/services/user-health.service';
 
 @Component({
@@ -11,54 +12,54 @@ import { UserHealthService } from 'src/app/services/user-health.service';
   templateUrl: './my-appointment.page.html',
   styleUrls: ['./my-appointment.page.scss'],
 })
+@NgModule({
+  imports: [
+    CommonModule, 
+    FormsModule,
+    BrowserModule
+  ],
+  declarations: [ModalMyHealthPage]
+})
 export class MyAppointmentPage implements OnInit {
-  public data = new Array<UserPills>();
+  public data;
   private dataSubscription: Subscription;
-  
-  private whereData = new Array();
-  private whereDataSubscription: Subscription;
   
   constructor(
     private modalControll: ModalController,
     private userService: UserHealthService
   ) {
-    this.dataSubscription = this.userService.getMyPills().subscribe(data => {
+    this.dataSubscription = this.userService.getUserData().subscribe(data => {
       this.data = data;
-      console.log(this.data)
-    })
-
-    this.whereDataSubscription = this.userService.getWhere().subscribe(data => {
-      this.whereData = data;
     })
   }
 
   ngOnInit() {
-    console.log(this.whereData)
+    console.log(this.data)
   }
 
   ngOnDestroy() {
     if(this.dataSubscription) this.dataSubscription.unsubscribe();
   }
   
-  async showModal(e) {        
+  async showModal(type, data) {
     let message: string;
-    switch (e) {
+    switch (type) {
       case 'pills':
-        message = "Meus Medicamentos";
+        message = "Meus Medicamentos";        
       break;
       case 'vaccine':
-        message = "Minhas Vacinas";
+        message = "Minhas Vacinas";        
       break;
-      case 'check-up':
+      case 'check_up':
         message = "Meus Exames";
       break;
       case 'professionals':
-        message = "Meus Profissionais";
+        message = "Meus Profissionais";        
       break;
       default:
         break;
     }
-    await this.presentModal(this.data[0].pill, message)
+    this.presentModal(data, type);
   }
 
   async presentModal( data:any, type:string) {
