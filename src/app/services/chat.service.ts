83bb5022d780f2cf1152ec5a6c4f28562804c13f
Ramon Cibas <ramon.cibas@hotmail.com>
2021-09-02 20:33:31 -0300

    identification correction

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -8,13 +8,16 @@ import { User } from '../interfaces/user';
 import { Message } from '../interfaces/chat';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
   currentUser: User = null;
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
-    this.afAuth.onAuthStateChanged(user => {
+  constructor(
+    private afAuth: AngularFireAuth, 
+    private afs: AngularFirestore
+  ) {
+    this.afAuth.onAuthStateChanged((user) => {
       this.currentUser = user;
     });
   }
@@ -27,16 +30,18 @@ export class ChatService {
       displayName: this.currentUser.displayName,
     });
   }
-  
+
   getChatMessage() {
     let users = [];
 
     return this.getUsers().pipe(
-      switchMap(res => {
+      switchMap((res) => {
         users = res;
-        return this.afs.collection('messages', ref => ref.orderBy('createdAt')).valueChanges({ idField: 'id' }) as Observable<Message[]>;
-      }),      
-      map(messages => {
+        return this.afs
+          .collection('messages', (ref) => ref.orderBy('createdAt'))
+          .valueChanges({ idField: 'id' }) as Observable<Message[]>;
+      }),
+      map((messages) => {
         for (let m of messages) {
           m.fromName = this.getUserForMsg('displayName', m.from, users);
           m.fromEmail = this.getUserForMsg('email', m.from, users);
@@ -44,15 +49,17 @@ export class ChatService {
         }
         return messages;
       })
-    )
+    );
   }
-  
+
   getUsers() {
-    return this.afs.collection('users').valueChanges({ idField: 'uid' }) as Observable<User[]>;
+    return this.afs
+      .collection('users')
+      .valueChanges({ idField: 'uid' }) as Observable<User[]>;
   }
 
   getUserForMsg(type, msgFromId, users: User[]): string {
-    for(let user of users) {
+    for (let user of users) {
       if (user.uid == msgFromId) {
         return type === 'displayName' ? user.displayName : user.email;
       }
