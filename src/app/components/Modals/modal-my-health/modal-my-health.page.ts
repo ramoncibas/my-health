99b5e4ea80264a21f5dd9cd5d84adb47dbd09fcf
Ramon Cibas <ramon.cibas@hotmail.com>
2021-09-08 20:32:04 -0300

    share data

--- src/app/components/Modals/modal-my-health/modal-my-health.page.ts
+++ src/app/components/Modals/modal-my-health/modal-my-health.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { ActionSheetController, LoadingController, ModalController } from '@ionic/angular';
 import { AppointmentService } from 'src/app/services/appointment.service';
 import { PillService } from 'src/app/services/pill.service';
 import { UserHealthService } from 'src/app/services/user-health.service';
@@ -18,34 +19,92 @@ export class ModalMyHealthPage implements OnInit {
   constructor(
     private loadingControll: LoadingController,
     private modalControll: ModalController,
+    private actionSheetController: ActionSheetController,
+    private socialSharing: SocialSharing,
     private pillsService: PillService,
     private userHealthService: UserHealthService,
-    private appointmentService: AppointmentService
+    private appointmentService: AppointmentService,
   ) {
     this.collectionId = this.userHealthService.getCurrentUserDocument().subscribe((data) => {
       this.collectionId = data.id;
     });
   }
 
-  ngOnInit() {
-    console.log(this.data)
-  }
+  ngOnInit() {}
   
   async closeModal() {
     await this.modalControll.dismiss();
-  }
- 
+  } 
+
+  // Share health data
   async shareItem(data) {
-    console.log(data);
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Contatar via:',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          text: 'WhatsApp',
+          icon: 'logo-whatsapp',
+          handler: () => {
+            this.socialSharing.shareViaWhatsAppToReceiver("+55", data).then(() => {
+              console.log('WhatsApp clicked');
+            }).catch(console.log)
+          },
+        },
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook',
+          handler: () => {
+            window.location.href = "https://www.facebook.com/"
+          },
+        },
+        {
+          text: 'Instagram',
+          icon: 'logo-instagram',
+          handler: () => {
+            window.location.href = "https://www.instagram.com/"            
+          },
+        },
+        {
+          text: 'LinkedIn',
+          icon: 'logo-linkedin',
+          handler: () => {
+            window.location.href = "https://www.linkedin.com/"            
+          },
+        },
+        {
+          text: 'Email',
+          icon: 'mail',
+          handler: () => {
+            this.socialSharing.shareViaEmail(data, "Help", ["ramon.cibas@hotmail.com"]).then(() => {
+              console.log("Email clicked!");
+            }).catch(console.log);
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log(role);
   }
 
   async deleteItem(data) {
     console.log(data);
   }
 
+  // Clean all data
   async cleanHistory(data) {
     await this.presentLoading();  
-    if(data[0].promotion) {
+    if(data[0].amount) {
       this.pillsService.deletePillHistory(this.collectionId);
     } else {
       this.appointmentService.deleteAppointmentHistory(this.collectionId);
